13544ee035adc526cc6de22ab3ecd5c7b03deb64
bia-pequeno <bia.2017pequeno@gmail.com>
2022-01-16 22:04:14 -0300

    atividade concluida

---
+++ script.js
@@ -0,0 +1,41 @@
+let idade = 18
+
+if (idade >= 18) {
+    console.log("parabens voce é de maior")
+}
+else{
+    console.log("voce é de menor")
+}
+let numero = 18
+let humano = true
+
+if (numero >=18 && humano == true ) {
+    console.log("voce é de maior")
+}
+
+let mês = "janeiro"
+
+if (mês == "janeiro") {
+    console.log("Feliz Aniversario")
+}
+else{
+    console.log("Não é seu aniversario")
+}
+
+let nome="Raquel"
+
+if (nome == "Beatriz") {
+    console.log("Seu nome começa com R")
+}
+else{
+    console.log("Seu nome não começa com R")
+}
+
+let nome2 = "Eduarda"
+
+if (nome2 == "Beatriz") {
+    console.log("Seu nome começa com E")
+}
+else{
+    console.log("Seu nome não começa com E")
+}
